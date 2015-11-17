795dd7823f1566ba91b76df27a69d00f2d9c7a90
Kris Szafranski <kdszafranski@gmail.com>
2015-11-17 11:27:04 -0600

    ngrouting working with api

---
+++ server/routes/addresses.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var router = express.Router();
+var pg = require('pg');
+
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/orderme';
+
+// Returns the entire list of people
+router.get("/", function(req, res) {
+    var results = [];
+    var criteria = {
+        userID: req.query.userID
+    };
+
+    pg.connect(connectionString, function (err, client, done) {
+        var query = "";
+        if(criteria) {
+            query = client.query("SELECT * FROM addresses WHERE user_id = $1", [criteria.userID]);
+        } else {
+            query = client.query("SELECT * FROM addresses");
+        }
+
+        // Stream results back one row at a time
+        query.on('row', function (row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function () {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+module.exports = router;
